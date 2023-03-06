3bf1246cca8aae8f99982e94865e1808801f9b08
hkrstev <103205287+hkrstev@users.noreply.github.com>
2023-03-05 23:52:52 -0600

    Tested Material UI by adding a card and a button on the homepage...

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActions from '@mui/material/CardActions';
+
+
+
+const Home = () => (
+  <>
+    <h1> Tca Trouble </h1>
+    <h2> Companion App </h2>
+    
+    <Card>
+      <CardContent>
+        <Typography variant="h3" component="div">
+          Play
+        </Typography>
+        <Typography variant="h5" component="div">
+          Play a game of TCA Trouble
+        </Typography>
+        <CardActions style={{justifyContent: 'center'}}>
+          <Button variant="contained" size="large">Play</Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+    
+  
+  </>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
