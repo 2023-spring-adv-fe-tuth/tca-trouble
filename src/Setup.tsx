4b8d4ffc8ecc965d89caf15784ce363b4d797ad9
hkrstev <103205287+hkrstev@users.noreply.github.com>
2023-03-30 01:15:42 -0500

    updated setup so that user can't select more than 4 players

--- src/Setup.tsx
+++ src/Setup.tsx
@@ -1,97 +1,110 @@
 import Button from "@mui/material/Button/Button";
-
 import { useState } from "react";
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 import { SetupInfo } from "./front-end-model";
-
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-
-import Box from '@mui/material/Box';
-import Input from '@mui/material/Input';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Box from "@mui/material/Box";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 export interface SetupProps {
-    previousPlayers: string[];
-    setSetupInfo: (si: SetupInfo) => void;
+  previousPlayers: string[];
+  setSetupInfo: (si: SetupInfo) => void;
 }
 
-export const Setup: React.FC <SetupProps> = ({ previousPlayers, setSetupInfo}) => {
+export const Setup: React.FC<SetupProps> = ({ previousPlayers, setSetupInfo }) => {
+  const nav = useNavigate();
 
-    const nav = useNavigate ();
+  const [chosenPlayers, setChosenPlayers] = useState(
+    previousPlayers.map((x) => ({ name: x, checked: false }))
+  );
 
-    const [chosenPlayers, setChosenPlayers] = useState(
-        previousPlayers.map (x => ({name: x, checked: false}))
+  const [addPlayer, setAddPlayer] = useState("");
+
+  const togglePlayer = (name: string) =>
+    setChosenPlayers(
+      chosenPlayers.map((x) => ({
+        ...x,
+        checked: x.name === name ? !x.checked : x.checked,
+      }))
     );
 
-    const [addPlayer, setAddPlayer] = useState ("");
+  const startGame = () => {
+    setSetupInfo({
+      start: new Date().toISOString(),
+      chosenPlayers: chosenPlayers.filter((x) => x.checked).map((x) => x.name),
+    });
+    nav("/play");
+  };
 
-    const togglePlayer = (name: string) => setChosenPlayers(
-        chosenPlayers.map(x => ({
-            ...x, checked: x.name === name ? !x.checked : x.checked
-        }))
-    );
+  const validateAddNewPlayer = () => {
+    if (
+      addPlayer.length == 0 ||
+      chosenPlayers.some((x) => x.name.localeCompare(addPlayer) == 0)
+    ) {
+      return;
+    }
+    setChosenPlayers([
+      ...chosenPlayers,
+      {
+        name: addPlayer,
+        checked: true,
+      },
+    ]);
+    setAddPlayer("");
+  };
 
-    const startGame = () => {
-        setSetupInfo({
-            start: new Date().toISOString(),
-            chosenPlayers: chosenPlayers.filter(x => x.checked).map(x => x.name)
-        });
-        nav ("/play");
-    };
+  const numSelected = chosenPlayers.filter((x) => x.checked).length;
+  const maxPlayers = 4;
 
-    const validateAddNewPlayer = () => {
-        if (
-            addPlayer.length == 0
-            || chosenPlayers.some(x => x.name.localeCompare(addPlayer) == 0)
-        ) {
-            return;
-        }
-        setChosenPlayers([
-            ...chosenPlayers,
-            {
-                name: addPlayer, 
-                checked: true
+  return (
+    <>
+      <h2>Setup</h2>
+
+      <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+        <AccountCircle sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+        <TextField
+          id="input-with-sx"
+          label="Enter your name"
+          variant="standard"
+          value={addPlayer}
+          onChange={(e) => setAddPlayer(e.target.value)}
+        />
+
+        <Button variant="contained" size="large" onClick={validateAddNewPlayer}>
+          Add Player
+        </Button>
+      </Box>
+
+      {chosenPlayers.map((x) => (
+        <FormGroup key={x.name}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={x.checked}
+                onChange={() => togglePlayer(x.name)}
+                disabled={numSelected === maxPlayers && !x.checked}
+              />
             }
-        ]
-        );
-        setAddPlayer("");
-    };
+            label={x.name}
+          />
+        </FormGroup>
+      ))}
 
-
-    return(
-        <>
-            <h2>Setup</h2>
-            
-            <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                <AccountCircle sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                <TextField id="input-with-sx" label="Enter your name" variant="standard" value={addPlayer} onChange={(e) => setAddPlayer(e.target.value)}/>
-                
-                <Button variant="contained" size="large" onClick={validateAddNewPlayer}>
-                    Add Player
-                </Button>
-                
-            </Box>
-    
-            
-            {
-                chosenPlayers.map(x =>(
-                    <FormGroup>
-                         <FormControlLabel
-                            control={<Checkbox checked={x.checked} 
-                            onChange={() => togglePlayer(x.name)} />}
-                            label={x.name}
-                        />
-                    </FormGroup>
-                )) 
-            }
-
-            <Button variant="contained" size="large" 
+            <Alert severity="error">
+                <AlertTitle><b> Trouble </b> can be played with maximum 4 players.</AlertTitle>
+               
+            </Alert>
+      
+            <Button variant="contained" size="large"
                 onClick={startGame}>Start Game
             </Button>
         </>
